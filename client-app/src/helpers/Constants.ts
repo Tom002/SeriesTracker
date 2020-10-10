e210e09811624b1b124e3aeed5fd83e87c302de2
Tom <tamas.princz3@gmail.com>
2020-10-10 23:25:58 +0200

    Minor changes

--- client-app/src/helpers/Constants.ts
+++ client-app/src/helpers/Constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-    public static identityServer = 'http://localhost:5000/';
+    public static identityServer = 'http://51.124.148.39/';
     public static clientId = 'react_client';
     public static clientRoot = 'http://localhost:3000/';
     public static clientScope = 'openid profile email api';
